32743e25c7b46f2b4901a05885fcdbdbfb20bbe9
Alexander <lolalex64@outlook.com>
2024-07-18 01:18:54 +0200

    Initial commit

---
+++ src/main/java/net/shycad/taskcrafter/TaskCrafter.java
@@ -0,0 +1,16 @@
+package net.shycad.taskcrafter;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskCrafter implements ModInitializer {
+    public static final String MOD_ID = "taskcrafter";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/shycad/taskcrafter/TaskCrafterClient.java
@@ -0,0 +1,10 @@
+package net.shycad.taskcrafter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TaskCrafterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
